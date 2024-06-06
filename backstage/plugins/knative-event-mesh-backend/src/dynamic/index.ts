83bb64fa9ec43d25928e6e42d6e525e8629b8dc5
Ali Ok <aliok@redhat.com>
2024-06-06 14:40:52 +0300

    Update backstage/plugins/knative-event-mesh-backend/src/dynamic/index.ts

--- backstage/plugins/knative-event-mesh-backend/src/dynamic/index.ts
+++ backstage/plugins/knative-event-mesh-backend/src/dynamic/index.ts
@@ -3,6 +3,9 @@ import {CatalogClient} from "@backstage/catalog-client";
 
 import {KnativeEventMeshProcessor, KnativeEventMeshProvider} from '../providers';
 
+// This is mainly to provide dynamic plugin support for backstage's legacy backend. During the plugin loading phase, it'll use this dynamicPluginInstaller.
+// https://github.com/backstage/backstage/blob/master/packages/backend-dynamic-feature-service/src/manager/types.ts
+// It's deprecated since the legacy backend is now deprecated, but it's probably still nice to have in case someone wants to install on the legacy backend for some reason.
 export const dynamicPluginInstaller:BackendDynamicPluginInstaller = {
     kind: 'legacy',
     async catalog(builder:any, env:any) {
