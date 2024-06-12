7e63d4edb7061f9b5eaace660d2948cdd640daa3
Ali Ok <aliok@redhat.com>
2024-06-12 14:31:08 +0300

    Get rid of listers and other injection
    Signed-off-by: Ali Ok <aliok@redhat.com>

--- backends/pkg/reconciler/eventmesh/controller.go
+++ backends/pkg/reconciler/eventmesh/controller.go
@@ -3,10 +3,12 @@ package eventmesh
 import (
 	"context"
 	"log"
+	"net/http"
 
 	"k8s.io/client-go/rest"
 
-	"knative.dev/eventing/pkg/kncloudevents"
+	"github.com/gorilla/mux"
+
 	"knative.dev/pkg/logging"
 )
 
@@ -16,6 +18,7 @@ func NewController(ctx context.Context) {
 
 	logger.Infow("Starting eventmesh-backend controller")
 
+	// TODO: does not stop with SIGTERM
 	startWebServer(ctx)
 }
 
@@ -25,17 +28,28 @@ func startWebServer(ctx context.Context) {
 
 	logger.Infow("Starting eventmesh-backend webserver")
 
-	noTokenConfig, err := rest.InClusterConfig()
-	if err != nil {
-		log.Fatalf("Error getting in-cluster config: %v", err)
-	}
+	r := mux.NewRouter()
+	r.Use(commonMiddleware)
 
+	noTokenConfig, err := rest.InClusterConfig()
 	noTokenConfig.BearerToken = ""
 	noTokenConfig.Username = ""
 	noTokenConfig.Password = ""
 	noTokenConfig.BearerTokenFile = ""
 
-	r := kncloudevents.NewHTTPEventReceiver(8080)
-	err = r.StartListen(ctx, HttpHandler{ctx, noTokenConfig})
-	log.Fatal(err)
+	if err != nil {
+		log.Fatalf("Error getting in-cluster config: %v", err)
+	}
+
+	r.HandleFunc("/", HttpHandler(ctx, noTokenConfig)).Methods("GET")
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func commonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
