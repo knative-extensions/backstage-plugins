c4145f03553413fa2e267c33abc254e313c24412
Ali Ok <aliok@redhat.com>
2024-09-18 11:09:39 +0300

    Introduce KUBECONFIG env var for debugging purposes
    Signed-off-by: Ali Ok <aliok@redhat.com>

--- backends/pkg/reconciler/eventmesh/controller.go
+++ backends/pkg/reconciler/eventmesh/controller.go
@@ -3,8 +3,11 @@ package eventmesh
 import (
 	"context"
 	"log"
+	"os"
 
 	"k8s.io/client-go/rest"
+
+	"k8s.io/client-go/tools/clientcmd"
 
 	"knative.dev/eventing/pkg/kncloudevents"
 	"knative.dev/pkg/logging"
@@ -25,9 +28,22 @@ func startWebServer(ctx context.Context) {
 
 	logger.Infow("Starting eventmesh-backend webserver")
 
-	noTokenConfig, err := rest.InClusterConfig()
-	if err != nil {
-		log.Fatalf("Error getting in-cluster config: %v", err)
+	var noTokenConfig *rest.Config
+	var err error
+
+	// if KUBECONFIG env var is present, use it, instead of in-cluster config
+	// this is especially useful for local development
+	kubeConfigPath := os.Getenv("KUBECONFIG")
+	if kubeConfigPath != "" {
+		noTokenConfig, err = clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+		if err != nil {
+			log.Fatalf("Error building kubeconfig: %v", err)
+		}
+	} else {
+		noTokenConfig, err = rest.InClusterConfig()
+		if err != nil {
+			log.Fatalf("Error getting in-cluster config: %v", err)
+		}
 	}
 
 	noTokenConfig.BearerToken = ""
