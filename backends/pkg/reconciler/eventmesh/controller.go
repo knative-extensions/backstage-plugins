739042432beb89ece1c99e998e01d6a60e2f335f
Ali Ok <aliok@redhat.com>
2024-09-18 14:34:40 +0300

    Use injection to accept KUBECONFIG env var
    Signed-off-by: Ali Ok <aliok@redhat.com>

--- backends/pkg/reconciler/eventmesh/controller.go
+++ backends/pkg/reconciler/eventmesh/controller.go
@@ -2,15 +2,10 @@ package eventmesh
 
 import (
 	"context"
-	"log"
-	"os"
-
-	"k8s.io/client-go/rest"
-
-	"k8s.io/client-go/tools/clientcmd"
-
 	"knative.dev/eventing/pkg/kncloudevents"
+	"knative.dev/pkg/injection"
 	"knative.dev/pkg/logging"
+	"log"
 )
 
 func NewController(ctx context.Context) {
@@ -28,23 +23,7 @@ func startWebServer(ctx context.Context) {
 
 	logger.Infow("Starting eventmesh-backend webserver")
 
-	var noTokenConfig *rest.Config
-	var err error
-
-	// if KUBECONFIG env var is present, use it, instead of in-cluster config
-	// this is especially useful for local development
-	kubeConfigPath := os.Getenv("KUBECONFIG")
-	if kubeConfigPath != "" {
-		noTokenConfig, err = clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-		if err != nil {
-			log.Fatalf("Error building kubeconfig: %v", err)
-		}
-	} else {
-		noTokenConfig, err = rest.InClusterConfig()
-		if err != nil {
-			log.Fatalf("Error getting in-cluster config: %v", err)
-		}
-	}
+	noTokenConfig := injection.ParseAndGetRESTConfigOrDie()
 
 	noTokenConfig.BearerToken = ""
 	noTokenConfig.Username = ""
@@ -52,6 +31,6 @@ func startWebServer(ctx context.Context) {
 	noTokenConfig.BearerTokenFile = ""
 
 	r := kncloudevents.NewHTTPEventReceiver(8080)
-	err = r.StartListen(ctx, HttpHandler{ctx, noTokenConfig})
+	err := r.StartListen(ctx, HttpHandler{ctx, noTokenConfig})
 	log.Fatal(err)
 }
