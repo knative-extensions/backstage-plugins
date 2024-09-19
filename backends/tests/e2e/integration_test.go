08f8689102f7427d10cc07c925e864f126fe99c9
Ali Ok <aliok@redhat.com>
2024-09-19 14:16:15 +0300

    E2E tests for backend security (#85)
    * Added e2e tests
    
    Signed-off-by: Ali Ok <aliok@redhat.com>
    
    * Check in vendored files
    
    Signed-off-by: Ali Ok <aliok@redhat.com>
    
    * Actually run the tests
    
    Signed-off-by: Ali Ok <aliok@redhat.com>
    
    * goimports
    
    Signed-off-by: Ali Ok <aliok@redhat.com>
    
    ---------
    
    Signed-off-by: Ali Ok <aliok@redhat.com>

--- backends/tests/e2e/integration_test.go
+++ backends/tests/e2e/integration_test.go
@@ -4,9 +4,71 @@
 package e2e
 
 import (
+	"context"
 	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubeclient "knative.dev/pkg/client/injection/kube/client"
+	"knative.dev/reconciler-test/pkg/k8s"
+	"knative.dev/reconciler-test/pkg/knative"
+
+	"knative.dev/reconciler-test/pkg/environment"
+	"knative.dev/reconciler-test/pkg/eventshub"
+	"knative.dev/reconciler-test/pkg/eventshub/assert"
+	"knative.dev/reconciler-test/pkg/feature"
 )
 
-func TestSample(t *testing.T) {
-	t.Log("All good")
+func TestIntegration(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace("knative-eventing"),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+	env.Test(ctx, t, VerifyBackstageBackendAuthentication())
+}
+
+func VerifyBackstageBackendAuthentication() *feature.Feature {
+
+	f := feature.NewFeature()
+
+	authenticatedClientName := feature.MakeRandomK8sName("authenticated-client")
+	unauthenticatedClientName := feature.MakeRandomK8sName("unauthenticated-client")
+	SANamespace := "eventmesh-backend-user-namespace"
+	SecretName := "eventmesh-backend-user-secret"
+
+	f.Setup("request with authenticated client", func(ctx context.Context, t feature.T) {
+		secret, err := kubeclient.Get(ctx).CoreV1().Secrets(SANamespace).Get(ctx, SecretName, metav1.GetOptions{})
+		if err != nil {
+			t.Fatal("Failed to get secret", err)
+		}
+
+		token := string(secret.Data["token"])
+
+		eventshub.Install(authenticatedClientName,
+			eventshub.StartSenderURL("http://eventmesh-backend.knative-eventing.svc.cluster.local:8080"),
+			eventshub.InputHeader("Authorization", "Bearer "+token),
+			eventshub.InputMethod("GET"),
+		)(ctx, t)
+	})
+	f.Setup("request with unauthenticated client", eventshub.Install(
+		unauthenticatedClientName,
+		eventshub.StartSenderURL("http://eventmesh-backend.knative-eventing.svc.cluster.local:8080"),
+		eventshub.InputHeader("Foo", "Bar"),
+		eventshub.InputMethod("GET")),
+	)
+
+	f.Assert("assert response with authenticated client", assert.OnStore(authenticatedClientName).
+		Match(assert.MatchKind(eventshub.EventResponse)).
+		Match(assert.MatchStatusCode(200)).
+		AtLeast(1))
+	f.Assert("assert response with unauthenticated client", assert.OnStore(unauthenticatedClientName).
+		Match(assert.MatchKind(eventshub.EventResponse)).
+		Match(assert.MatchStatusCode(401)).
+		AtLeast(1))
+
+	return f
 }
---
+++ backends/tests/e2e/main_test.go
@@ -0,0 +1,41 @@
+//go:build e2e
+// +build e2e
+
+/*
+ * Copyright 2024 The Knative Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package e2e
+
+import (
+	"os"
+	"testing"
+
+	_ "knative.dev/pkg/system/testing"
+	"knative.dev/reconciler-test/pkg/environment"
+)
+
+// global is the singleton instance of GlobalEnvironment. It is used to parse
+// the testing config for the test run. The config will specify the cluster
+// config as well as the parsing level and state flags.
+var global environment.GlobalEnvironment
+
+// TestMain is the first entry point for `go test`.
+func TestMain(m *testing.M) {
+	global = environment.NewStandardGlobalEnvironment()
+
+	// Run the tests.
+	os.Exit(m.Run())
+}
